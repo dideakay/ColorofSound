6d3948b461da3f11626ecb339d6242ff2e852293
dideakay <dideakay@gmail.com>
2022-09-25 10:45:17 +0300

    OOP Structure

--- gui1.py
+++ gui1.py
@@ -1,106 +1,99 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import ttk
 import numpy as np
 import pyaudio
 
-NOTE_MIN = 60       # C4
-NOTE_MAX = 69       # A4
-FSAMP = 22050       # Sampling frequency in Hz
-FRAME_SIZE = 2048   # How many samples per frame?
-FRAMES_PER_FFT = 16 # FFT takes average across how many frames?
-listen = False
+class AudioListener():
+    def __init__(self, *args, **kwargs):
+        print(args)
+        self.NOTE_MIN = 60       # C4
+        self.NOTE_MAX = 69       # A4
+        self.FSAMP = 22050       # Sampling frequency in Hz
+        self.FRAME_SIZE = 2048   # How many samples per frame?
+        self.FRAMES_PER_FFT = 16 # FFT takes average across how many frames?
 
-SAMPLES_PER_FFT = FRAME_SIZE*FRAMES_PER_FFT
-FREQ_STEP = float(FSAMP)/SAMPLES_PER_FFT
+        self.SAMPLES_PER_FFT = self.FRAME_SIZE*self.FRAMES_PER_FFT
+        self.FREQ_STEP = float(self.FSAMP)/self.SAMPLES_PER_FFT
 
-NOTE_NAMES = 'C C# D D# E F F# G G# A A# B'.split()
+        self.NOTE_NAMES = 'C C# D D# E F F# G G# A A# B'.split()
 
-def freq_to_number(f): return 69 + 12*np.log2(f/440.0)
-def number_to_freq(n): return 440 * 2.0**((n-69)/12.0)
-def note_name(n): return NOTE_NAMES[n % 12] + str(n/12 - 1)
+        self.imin = max(0, int(np.floor(self.note_to_fftbin(self.NOTE_MIN-1))))
+        self.imax = min(self.SAMPLES_PER_FFT, int(np.ceil(self.note_to_fftbin(self.NOTE_MAX+1))))
 
-def note_to_fftbin(n): return number_to_freq(n)/FREQ_STEP
-imin = max(0, int(np.floor(note_to_fftbin(NOTE_MIN-1))))
-imax = min(SAMPLES_PER_FFT, int(np.ceil(note_to_fftbin(NOTE_MAX+1))))
+        # Allocate space to run an FFT. 
+        self.buf = np.zeros(self.SAMPLES_PER_FFT, dtype=np.float32)
 
+        # Initialize audio
+        self.stream = pyaudio.PyAudio().open(format=pyaudio.paInt16,
+                                        channels=1,
+                                        rate=self.FSAMP,
+                                        input=True,
+                                        frames_per_buffer=self.FRAME_SIZE)
 
+       
+    def freq_to_number(self, f): return 69 + 12*np.log2(f/440.0)
+    def number_to_freq(self, n): return 440 * 2.0**((n-69)/12.0)
+    def note_name(self, n): return self.NOTE_NAMES[n % 12] + str(n/12 - 1)
 
-# Allocate space to run an FFT. 
-buf = np.zeros(SAMPLES_PER_FFT, dtype=np.float32)
-num_frames = 0
+    def note_to_fftbin(self, n): return self.number_to_freq(n)/self.FREQ_STEP
 
-# Initialize audio
-stream = pyaudio.PyAudio().open(format=pyaudio.paInt16,
-                                channels=1,
-                                rate=FSAMP,
-                                input=True,
-                                frames_per_buffer=FRAME_SIZE)
+    def listenLoop(self):
+        num_frames = 0
+        self.stream.start_stream()
 
+        # Create Hanning window function
+        window = 0.5 * (1 - np.cos(np.linspace(0, 2*np.pi, self.SAMPLES_PER_FFT, False)))
 
-def listenLoop(listenGlobal):
-    global listen
-    num_frames = 0
-    stream.start_stream()
-    
-    # Create Hanning window function
-    window = 0.5 * (1 - np.cos(np.linspace(0, 2*np.pi, SAMPLES_PER_FFT, False)))
+        while (self.stream.is_active()):
+            
+            # Shift the buffer down and new data in
+            self.buf[:-self.FRAME_SIZE] = self.buf[self.FRAME_SIZE:]
+            self.buf[-self.FRAME_SIZE:] = np.fromstring(self.stream.read(self.FRAME_SIZE), np.int16)
 
-    while (stream.is_active() and listen):
+            # Run the FFT on the windowed buffer
+            fft = np.fft.rfft(self.buf * window)
 
-        # Shift the buffer down and new data in
-        buf[:-FRAME_SIZE] = buf[FRAME_SIZE:]
-        buf[-FRAME_SIZE:] = np.fromstring(stream.read(FRAME_SIZE), np.int16)
+            # Get frequency of maximum response in range
+            freq = (np.abs(fft[self.imin:self.imax]).argmax() + self.imin) * self.FREQ_STEP
 
-        # Run the FFT on the windowed buffer
-        fft = np.fft.rfft(buf * window)
+            # Get note number and nearest note
+            n = self.freq_to_number(freq)
+            n0 = int(round(n))
 
-        # Get frequency of maximum response in range
-        freq = (np.abs(fft[imin:imax]).argmax() + imin) * FREQ_STEP
+            # Console output once we have a full buffer
+            num_frames += 1
 
-        # Get note number and nearest note
-        n = freq_to_number(freq)
-        n0 = int(round(n))
+            if num_frames >= self.FRAMES_PER_FFT:
+                print ('freq: {:7.2f} Hz     note: {:>3s} {:+.2f}'.format(
+                    freq, self.note_name(n0), n-n0))
 
-        # Console output once we have a full buffer
-        num_frames += 1
+class App(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
 
-        if num_frames >= FRAMES_PER_FFT:
-            print ('freq: {:7.2f} Hz     note: {:>3s} {:+.2f}'.format(
-                freq, note_name(n0), n-n0))
+        # configure the root window
+        self.title('Dides Tuner')
+        self.geometry('300x50')
+
+        # label
+        self.label = ttk.Label(self, text='Hello, Press to start! You can read the notes from console output!')
+        self.label.pack()
         
+        # button
+        self.btn = tk.Button(self, text="Press", command=lambda: button_clicked(self))
+        self.btn.pack()
 
-def toggleListen():
-    global listen
-    if listen == True:
-        listen = False
-    else:
-        listen = True
-    print(listen)
+        listener = AudioListener()
+    
+        def startListening(self):
+            listener.__init__(listener)
+            listener.listenLoop()
 
-def getListen():
-     global listen
-     return listen
+        def button_clicked(self):
+            startListening(self)
 
-
-root = Tk()
-
-button1 = Button(root, text="start / stop",  command=toggleListen)
-button1.grid(row=0, column=0)
-
-button1 = Button(root, text="listening", command= lambda: listenLoop(listen))
-button1.grid(row=0, column=1)
-
-button1 = Button(root, text="stop")
-button1.grid(row=0, column=2)
-
-myLabel1 = Label(root, text="is listening:")
-entryField = Entry(root, textvariable=listen)
-entryField.grid(row=1, column=0)
-myLabel2 = Label(root, text=getListen)
-
-
-
-
-myLabel1.grid(row=1, column=0)
-myLabel2.grid(row=1, column=1)
-
-root.mainloop() 
+        
+            
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
