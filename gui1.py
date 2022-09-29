8daac70dc422f124f3ee6eadc0ec5c4cc909a8cc
dideakay <dideakay@gmail.com>
2022-09-29 19:10:07 +0300

    Add background color change

--- gui1.py
+++ gui1.py
@@ -11,103 +11,24 @@ import pyaudio
 from pyaudio import PyAudio, paInt16
 
 frequency = "Not Listening"
-class AudioListener2():
-    is_listening = True
-    global labelGlobal
-    def __init__(self, *args, **kwargs):
-        
-        self.NOTE_MIN = 40      # E2
-        self.NOTE_MAX = 88       # E6
-        #self.NOTE_MIN_FREQ =
-        self.NOTE_MAX_FREQ =  1318.51 # E6
-
-        self.FSAMP = 48000       # Sampling frequency in Hz
-        self.FRAME_SIZE = 1024   # How many samples per frame?
-        self.FRAMES_PER_FFT = 50 # FFT takes average across how many frames?
-
-        self.SAMPLES_PER_FFT = self.FRAME_SIZE*self.FRAMES_PER_FFT
-        self.FREQ_STEP = float(self.FSAMP)/self.SAMPLES_PER_FFT
-        
-        self.NOTE_NAMES = 'C C# D D# E F F# G G# A A# B'.split()
-
-        self.imin = max(0, int(np.floor(self.note_to_fftbin(self.NOTE_MIN-1)))) # n = 1 if note min is even
-        self.imax = min(self.SAMPLES_PER_FFT, int(np.ceil(self.note_to_fftbin(self.NOTE_MAX))))
-
-        # Allocate space to run an FFT. 
-        self.buf = np.zeros(self.SAMPLES_PER_FFT, dtype=np.float32)
-
-        # Initialize audio
-        self.stream = pyaudio.PyAudio().open(format=pyaudio.paInt16,
-                                        channels=1,
-                                        rate=self.FSAMP,
-                                        input=True,
-                                        output=False,
-                                        frames_per_buffer=self.FRAME_SIZE)
-
-       
-    def freq_to_number(self, f): return 69 + 12*np.log2(f/440)
-    def number_to_freq(self, n): return 440 * 2.0**((n-69)/12.0)
-    def note_name(self, n): return self.NOTE_NAMES[n % 12] + str(n/12 - 1)
-
-    def note_to_fftbin(self, n): return self.number_to_freq(n)/self.FREQ_STEP
-
-    def change_color(self):
-            global bgcolor
-            bgcolor = '#40E0D0'
-
-    def listenLoop(self, mainApp):
-        global frequency
-        num_frames = 0
-        self.stream.start_stream()
-
-        # Create Hanning window function
-        window = 0.5 * (1 - np.cos(np.linspace(0, 2*np.pi, self.SAMPLES_PER_FFT, False)))
-
-        while (self.stream.is_active() and self.is_listening):
-            
-            # Shift the buffer down and new data in
-            self.buf[:-self.FRAME_SIZE] = self.buf[self.FRAME_SIZE:]
-            self.buf[-self.FRAME_SIZE:] = np.fromstring(self.stream.read(self.FRAME_SIZE), np.int16)
-
-            # Run the FFT on the windowed buffer
-            fft = np.fft.rfft(self.buf * window)
-
-            # Get frequency of maximum response in range
-            freq = (np.abs(fft[self.imin:self.imax]).argmax() + self.imin) * self.FREQ_STEP
-
-            # Get note number and nearest note
-            n = self.freq_to_number(freq)
-            n0 = int(round(n))
-
-            # Console output once we have a full buffer
-            num_frames += 1
-
-            if num_frames >= self.FRAMES_PER_FFT:
-                frequency = 'freq: {:7.2f} Hz     note: {:>3s} {:+.2f}'.format(freq, self.note_name(n0), n-n0)
-                print(frequency)
-                self.change_color()
 
 class AudioListener():
     is_listening = True
-    global labelGlobal
-        # settings: (are tuned for best detecting string instruments like guitar)
+    global note_name_global
+    note_name_global="A"
+    
+
     SAMPLING_RATE = 48000  # mac hardware: 44100, 48000, 96000
     CHUNK_SIZE = 1024  # number of samples
     BUFFER_TIMES = 50  # buffer length = CHUNK_SIZE * BUFFER_TIMES
     ZERO_PADDING = 3  # times the buffer length
     NUM_HPS = 3  # Harmonic Product Spectrum
     a4_freq = 440
-    
-
-    # overall frequency accuracy (step-size):  SAMPLING_RATE / (CHUNK_SIZE * BUFFER_TIMES * (1 + ZERO_PADDING)) Hz
-    #               buffer length in seconds:  (CHUNK_SIZE * BUFFER_TIMES) / SAMPLING_RATE sec
 
     NOTE_NAMES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
     def __init__(self, *args, **kwargs):
         
         self.main_path = os.path.dirname(os.path.abspath(__file__))
-       # self.frequency_queue = ProtectedList()
-        #self.queue = queue  # queue should be instance of ProtectedList (threading_helper.ProtectedList)
         self.buffer = np.zeros(self.CHUNK_SIZE * self.BUFFER_TIMES)
         self.hanning_window = np.hanning(len(self.buffer))
         self.running = False
@@ -139,18 +60,46 @@ class AudioListener():
     @staticmethod
     def number_to_note_name(number):
         """ converts a note number to a note name (for example: 69 returns 'A', 70 returns 'A#', ... ) """
-
-        return AudioListener.NOTE_NAMES[int(round(number) % 12)]
+        global note_name_global
+        note_name_global = AudioListener.NOTE_NAMES[int(round(number) % 12)] 
+        return note_name_global + str(number/12 - 1)
 
     @staticmethod
     def frequency_to_note_name(frequency, a4_freq):
         """ converts frequency to note name (for example: 440 returns 'A') """
-
+        global note_name_global
         number = AudioListener.frequency_to_number(frequency, a4_freq)
         note_name = AudioListener.number_to_note_name(number)
         return note_name
 
-        
+    @staticmethod
+    def note_to_rgb():
+        global bg_color
+        if(note_name_global=="C"):
+            bg_color='#28ff00'
+        elif(note_name_global=="C#"):
+            bg_color='#00ffe8'
+        elif(note_name_global=="D"):
+            bg_color='#007cff'
+        elif(note_name_global=="D#"):
+            bg_color='#0500ff'
+        elif(note_name_global=="E"):
+            bg_color='#4500ea'
+        elif(note_name_global=="F"):
+            bg_color='#57009e'
+        elif(note_name_global=="F#"):
+            bg_color='#740000'
+        elif(note_name_global=="G"):
+            bg_color='b30000'
+        elif(note_name_global=="G#"):
+            bg_color='#ee0000'
+        elif(note_name_global=="A"):
+            bg_color='#ff6300'
+        elif(note_name_global=="A#"):
+            bg_color='#ffec00'
+        else:
+            bg_color='#99ff00'
+           
     def listenLoop(self, mainApp):
         self.running = True
         global frequency
@@ -199,21 +148,19 @@ class AudioListener():
             # Get note number and nearest note
             n = self.frequency_to_number(freq, 440)
             n0 = int(round(n))
-
             # Console output once we have a full buffer
             num_frames += 1
+
+            AudioListener.note_to_rgb()
 
             if num_frames >= self.BUFFER_TIMES:
                 frequency = 'freq: {:7.2f} Hz     note: {:>3s} {:+.2f}'.format(freq, self.number_to_note_name(n0), n-n0)
                 print(frequency)
                 
-                
-
-        
-
 class App(tk.Tk):
     global frequency
     global bg_color
+    bg_color = '#40E0D0'
     def __init__(self, *args, **kwargs):
         super().__init__()
 
@@ -221,9 +168,9 @@ class App(tk.Tk):
         self.title('Dides Tuner')
         self.geometry('400x150')
         
-        bg_color = '#40E0D0'
-        self.configure(bg=bg_color)
-        self.after(100, self.update_bg)
+        
+        #self.configure(bg=bg_color)
+        #self.update_bg()
 
         # label
         self.label = tk.Label(self, text='Hello, Press to start!')
@@ -262,15 +209,17 @@ class App(tk.Tk):
     # update the label every 1 second 
         if(self.listener.is_listening == True):
             self.frequencyLabel.configure(text=frequency)
+            self.configure(bg=bg_color)
 
         # schedule another timer
         self.frequencyLabel.after(1000, self.update)
 
     def update_bg(self):
         self.configure(bg=bg_color)
-        self.after(100, self.update_bg)
+        print("----------------------------------------------------------------------")
+        print(bg_color)
+        self.frequencyLabel.after(100, self.update_bg)
 
-      
 if __name__ == "__main__":
     app = App()
     app.mainloop()
